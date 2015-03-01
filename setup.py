f44429c59c35abd85b8a180bc95b8829d79a487b
Mindey <mindey@qq.com>
2015-03-02 01:17:30 +0200

    Removed requirements.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(name='IdeaLib',
-      version='0.1',
+      version='0.1.1',
       description='',
       url='http://github.com/Mindey/IdeaLib',
       author='Mindey',
       author_email='mindey@gmail.com',
       license='MI',
-      install_requires=['pandas >= 0.14.1', 'numpy >= 1.8.2', 'matplotlib >= 1.3.1'],
       packages=['IdeaLib'],
       zip_safe=False)
